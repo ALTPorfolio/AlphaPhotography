ade42b4a6cdd02dc9fd09611c7f790255e5c04cb
AleksPeneva <alexemilpeneva@abv.bg>
2018-02-23 14:22:47 +0200

    navbar is fully functional

--- controller/header.js
+++ controller/header.js
@@ -2,6 +2,15 @@ $('.header-container').on('click', '#landscapes-btn', function() {
     $('.app-container').load('../views/portfolio/landscapes.html');
 });
 
-$('.header-container').on('click', '#portraits-btn', function () {
+$('.header-container').on('click', '#portraits-btn', function() {
     $('.app-container').load('../views/portfolio/portraits.html');
 });
+
+$('.header-container').on('click', '#home-btn', function() {
+    $('.app-container').load('../views/core/home.html');
+});
+
+$('.header-container').on('click', '#photosessions-btn', function() {
+    $('.app-container').load('../views/photosessions/photosessions.html');
+});
+
