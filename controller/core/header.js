69a6a4be5250c231e46ac510f158d050019bfd7f
Lubomir Yonchev <infra1515@gmail.com>
2018-02-24 13:34:26 +0200

    fixed misplaced folders/files.

--- controller/core/header.js
+++ controller/core/header.js
@@ -1,16 +1,19 @@
-$('.header-container').on('click', '#landscapes-btn', function() {
-    $('.app-container').load('../views/portfolio/landscapes.html');
-});
+$('.header-container').on('click', '#landscapes-btn', function () {
+    $('.app-container')
+        .load('../views/portfolio/landscapes.html', function () {
+            $.get('https://api.instagram.com/v1/users/self/media/recent/?access_token=7163448156.1677ed0.2dfbf24af3444d2bb44c2103bc40a019',
+                function (data) {
+                    // console.log(data.data[0].images.low_resolution.url);
 
-$('.header-container').on('click', '#portraits-btn', function() {
-    $('.app-container').load('../views/portfolio/portraits.html');
-});
-
-$('.header-container').on('click', '#home-btn', function() {
-    $('.app-container').load('../views/core/home.html');
-});
-
-$('.header-container').on('click', '#photosessions-btn', function() {
-    $('.app-container').load('../views/photosessions/photosessions.html');
+                    for (var i = 0; i < 3; i++) {
+                        var thumbnailTag = $('#thumbnail-tag' + i)
+                            .attr('src', data.data[i].images.low_resolution.url)
+                            .css({
+                                width: '300px',
+                                height: '300px'
+                            });
+                    }
+                });
+        });
 });
 
