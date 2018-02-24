ae687ec67232a5a4052354c0005c1e0f72cfc3cb
Lubomir Yonchev <infra1515@gmail.com>
2018-02-24 15:50:40 +0200

    succesfull visualisation of landscape pictures with a for-loop and DOM traversal.

--- controller/core/header.js
+++ controller/core/header.js
@@ -3,15 +3,28 @@ $('.header-container').on('click', '#landscapes-btn', function () {
         .load('../views/portfolio/landscapes.html', function () {
             $.get('https://api.instagram.com/v1/users/self/media/recent/?access_token=7163448156.1677ed0.2dfbf24af3444d2bb44c2103bc40a019',
                 function (data) {
-                    // console.log(data.data[0].images.low_resolution.url);
+
+                    //$('#thumbnail-tag0').each(function () {
+                    //    console.log(this);
+                    //    $(this).find('a').each(function () {
+                    //        this.setAttribute('href', data.data[0].images.low_resolution.url);
+                    //        $(this).find('img').each(function () {
+                    //            this.setAttribute('src', data.data[0].images.low_resolution.url);
+                    //        });
+                    //    });
+                    //});
 
                     for (var i = 0; i < 3; i++) {
-                        var thumbnailTag = $('#thumbnail-tag' + i)
-                            .attr('src', data.data[i].images.low_resolution.url)
-                            .css({
-                                width: '300px',
-                                height: '300px'
+                        $('#thumbnail-tag' + i).each(function () {
+                            console.log(this);
+                            $(this).find('a').each(function () {
+                                this.setAttribute('href', data.data[i].images.standard_resolution.url);
+                                $(this).find('img').each(function () {
+                                    this.setAttribute('src', data.data[i].standard_resolution.url)
+                                        ;
+                                });
                             });
+                        });
                     }
                 });
         });
