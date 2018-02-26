ccb21fb831a7534ca099154c8a24393b516fd45b
Unknown <dokhanhtung1993@gmail.com>
2018-02-27 00:47:57 +0200

    Testing local storage with different approach

--- controller/pages/inspire_us.js
+++ controller/pages/inspire_us.js
@@ -7,28 +7,30 @@ function inspire_us() {
         appContainer.load('../views/inspire-us/inspire-us.html', onLoad());
     }
 
-    function onLoad() {
-        localStorage.setItem('lsa', JSON.stringify(lsa));
-        var retrievedObject = JSON.parse(localStorage.getItem('lsa'));
+    function onLoad() {        
+        let submitButton = $('#submit-picture');
+        let pictureUrl = $('#picture-url');
+        let itemsArray = localStorage.getItem('items') ? JSON.parse(localStorage.getItem('items')) : [];
 
-        dict = {
-            "user-name": " ",
-            "picture-description": " ",
-            "picture-url": " "
-        };
+        localStorage.setItem('items',JSON.stringify(itemsArray));
+        const data = JSON.parse(localStorage.getItem('items'));
 
-        $('#submit-picture').on('click', function () {
-            $('input[type="text"]').each(function () {
-
-                var id = $(this).attr('id');
-                var value = $(this).val();
-                dict[id] = value;
-                retrievedObject.p
-                localStorage.setItem('lsa', JSON.stringify(lsa));
-
-                //localStorage.setItem(id, value);
-            })
+        submitButton.on('click',function(){
+            itemArray.push(pictureUrl.val);
+            localStorage.setItem("items", JSON.stringify(itemsArray));
+        })
+        data.forEach(item => {
+            let templateStr2 = `<div class="col-md-4">
+                                                <div id="thumbnail-tag0" class="thumbnail">
+                                                    <a href="${item}">
+                                                        <img src="${item}">
+                                                    </a>
+                                                </div>
+                                            </div>`;
+            $('#inspire-us').append(templateStr2);
         });
+
+
     }
 
     return {
