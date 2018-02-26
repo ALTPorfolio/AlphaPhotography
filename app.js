4053cbc6338a846be129b3fb17984b1ea3a9f16b
Lubomir Yonchev <infra1515@gmail.com>
2018-02-26 13:08:39 +0200

    Moved photosession request button to navbar(visible only on Photosession.html)

--- app.js
+++ app.js
@@ -4,26 +4,34 @@ $(function () {
 
     $('.header-container').load('./views/core/header.html', function () {
 
+        $("#request-btn").hide();
         headerContainer.on('click', '#home-btn', function () {
             appContainer.load('../views/core/home.html');
+            $("#request-btn").hide();
+
         });
 
         headerContainer.on('click', '#photosessions-btn', function () {
             appContainer.load('../views/photosessions/photosessions.html');
+            $("#request-btn").show();
         });
+
 
         headerContainer.on('click', '#portraits-btn', function () {
             const page = portraitsPage();
             page.loadHtml();
+            $("#request-btn").hide();
         });
 
         headerContainer.on('click', '#landscapes-btn', function () {
             const page = landscapesPage();
             page.loadHtml();
+            $("#request-btn").hide();
         })
 
         headerContainer.on('click', '#contacts-btn', function () {
             appContainer.load('../views/contacts/contacts.html');
+            $("#request-btn").hide();
         });
     });
     $('.app-container').load('./views/core/home.html', function () {
