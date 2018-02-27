b3013f4615895260e044196520365cb976a46fda
Unknown <dokhanhtung1993@gmail.com>
2018-02-27 13:09:43 +0200

    Adding 2 fields WIP

--- controller/pages/inspire_us.js
+++ controller/pages/inspire_us.js
@@ -8,20 +8,28 @@ function inspire_us() {
     function onLoad() {        
         let submitButton = $('#submit-picture');
         let pictureUrl = $('#picture-url');
+        let userName = $('user-name');
+        let pictureDescription = $('#picture-description')
         let itemsArray = localStorage.getItem('items') ? JSON.parse(localStorage.getItem('items')) : [];
 
         localStorage.setItem('items',JSON.stringify(itemsArray));
         let data = JSON.parse(localStorage.getItem('items'));
 
         submitButton.on('click',function(){
-            itemsArray.push(pictureUrl.val());
+
+            let submission = {
+                name: userName.val(),
+                pictureD: pictureDescription.val(),
+                url: pictureUrl.val()
+              };
+            itemsArray.push(submission); 
             localStorage.setItem("items", JSON.stringify(itemsArray));
         })
         data.forEach(item => {
             let templateStr2 = `<div class="col-md-4">
                                                 <div id="thumbnail-tag0" class="thumbnail">
-                                                    <a href="${item}">
-                                                        <img src="${item}">
+                                                    <a href="${item.url}">
+                                                        <img src="${item.url}">
                                                     </a>
                                                 </div>
                                             </div>`;
